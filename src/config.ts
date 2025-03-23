4b253e7bcd8e2f1a8963405de24f526ac5f94282
amiminn <tholabul.amin@gmail.com>
2025-03-24 01:00:17 +0700

    Update SMTP password in configuration

--- src/config.ts
+++ src/config.ts
@@ -7,4 +7,4 @@ export const MAXEMAILS: number = 10;
 export const SMTPHOST: string = "mail.amiminn.my.id";
 export const SMTPPORT: number = 587;
 export const SMTPUSER: string = "support@amiminn.my.id";
-export const SMTPPASS: string = "JLFGClcyvH";
+export const SMTPPASS: string = "9A5u7NP}r9";
